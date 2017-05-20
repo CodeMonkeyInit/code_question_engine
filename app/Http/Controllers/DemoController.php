01e177d1e29c5831a9cb1360d0977fe5497d5f43
Kirill <suhorukih@mail.ru>
2017-05-20 22:29:28 +0300

    поправил настройки бд

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -3,15 +3,37 @@
 
 namespace App\Http\Controllers;
 use CodeQuestionEngine\CodeTask;
+use Repositories\UnitOfWork;
+use DockerInfo;
+
 
 
 class DemoController
 {
 
+
+    private $_uow;
+
+    public function __construct(UnitOfWork $uow){
+
+        $this->_uow = $uow;
+    }
     public function docker(){
 
 
-       /* $task = new CodeTask(0
+        $info = new DockerInfo();
+
+
+        $info->setContainerId("test");
+        $info->setLang(1);
+        $info->setId(1);
+        $this->_uow->dockerInfos()->create($info);
+
+        $this->_uow->commit();
+
+        dd();
+
+        /* $task = new CodeTask(0
             ,1
             , 2
             , 3
